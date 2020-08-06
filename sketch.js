bc49524a2c931a33b418e9623b68cee356d747bc
SWAPNILXYZ <66053332+SWAPNILXYZ@users.noreply.github.com>
2020-08-06 19:32:10 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,64 +1,66 @@
+
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+function preload()
+{
+	
+}
+bobDiameter = this.radius;
 
-var engine, world;
-var box1, pig1;
-var backgroundImg,platform;
+function setup() {
+ 
+	createCanvas(800, 700);
 
-function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+    roof = new Ceiling(400,300,300,20);
+    
+    bob1 = new Bob(400,600,30);
+    bob2 = new Bob(340,600,30);
+    bob3 = new Bob(460,600,30);
+    bob4 = new Bob(520,600,30);
+    bob5 = new Bob(280,600,30);
+    
+    rope1 = new Rope(bob1.body,roof.body,-2,0);
+    rope2 = new Rope(bob2.body,roof.body,-60,0);
+    rope3 = new Rope(bob3.body,roof.body,60,0);
+    rope4 = new Rope(bob4.body,roof.body,120,0);
+    rope5 = new Rope(bob5.body,roof.body,-120,0);
+	Engine.run(engine);
+  
+}
+ 
+
+function draw() {
+  rectMode(CENTER);
+  background("white");
+
+  rope1.display();
+  rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
+ 
+  roof.display();
+  bob1.display();
+  bob2.display();
+  bob3.display();
+  bob4.display();
+  bob5.display();
+  drawSprites();
+ 
+}
+keyPressed();
+function keyPressed(){
+	if (keyCode === LEFT_ARROW){
+		Body.applyForce(bob5.body,bob5.body.position,{x:-0.2,y:0});
+	}
 }
 
-function setup(){
-    var canvas = createCanvas(1200,400);
-    engine = Engine.create();
-    world = engine.world;
 
 
-    ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
-
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
-
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
-
-    log3 =  new Log(810,180,300, PI/2);
-
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
-
-    bird = new Bird(100,100);
-
-}
-
-function draw(){
-    background(backgroundImg);
-    Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
-    box1.display();
-    box2.display();
-    ground.display();
-    pig1.display();
-    log1.display();
-
-    box3.display();
-    box4.display();
-    pig3.display();
-    log3.display();
-
-    box5.display();
-    log4.display();
-    log5.display();
-
-    bird.display();
-    platform.display();
-}
